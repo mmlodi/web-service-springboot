f1ed20587797dfb87e08c5c1b44f3776f6f50d4d
Lodi <mischellodi@gmail.com>
2021-06-29 15:13:25 -0300

    Camada de serviços e registro de componentes

--- src/main/java/com/loja/webservice/resources/UserResource.java
+++ src/main/java/com/loja/webservice/resources/UserResource.java
@@ -1,18 +1,36 @@
 package com.loja.webservice.resources;
 
 import com.loja.webservice.entities.User;
+import com.loja.webservice.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User u = new User(1L, "Suetam", "mateus@hotmail.com", "996585056", "123456");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();//User u = new User(1L, "Suetam", "mateus@hotmail.com", "996585056", "123456");
+        return ResponseEntity.ok().body(list);
     }
+
+    @GetMapping(value = "/{id}") //É uma requisição do tipo GET
+    public ResponseEntity<User> findById( @PathVariable Long id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+
+    }
+
+
+
 }
---
+++ src/main/java/com/loja/webservice/services/UserService.java
@@ -0,0 +1,27 @@
+package com.loja.webservice.services;
+
+import com.loja.webservice.entities.User;
+import com.loja.webservice.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired //Faz uma injeção de dependência leve, transparente ao programador
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+
+
+}
