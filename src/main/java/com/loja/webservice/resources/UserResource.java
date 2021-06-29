6bd00689234db2335549ef4a5b1c6f34acb4aae2
Lodi <mischellodi@gmail.com>
2021-06-29 11:34:18 -0300

    Teste H2, profile e JPA feitos com sucesso

--- src/main/java/com/loja/webservice/resources/UserResource.java
+++ src/main/java/com/loja/webservice/resources/UserResource.java
@@ -7,12 +7,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value="/users")
+@RequestMapping(value = "/users")
 public class UserResource {
 
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User u = new User(1L, "Suetam", "mateus@hotmail.com", "996585056", "123456" );
+    public ResponseEntity<User> findAll() {
+        User u = new User(1L, "Suetam", "mateus@hotmail.com", "996585056", "123456");
         return ResponseEntity.ok().body(u);
     }
 }
